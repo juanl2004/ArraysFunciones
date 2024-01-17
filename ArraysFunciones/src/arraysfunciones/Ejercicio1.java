3f90670e3786f95bcca22e2502c77c0c473f6e07
jlbarrionuevo <jlbarrionuevo@106-06.info2>
2024-01-17 10:25:59 +0100

    Ejercicios

--- ArraysFunciones/src/arraysfunciones/Ejercicio1.java
+++ ArraysFunciones/src/arraysfunciones/Ejercicio1.java
@@ -3,8 +3,22 @@ package arraysfunciones;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int tabla[] = {4, 2, 8, 6, 10};
+
+		int resultado = Ejercicio1.suma(tabla);
+		
+		System.out.println("Suma --> " + resultado);
 	}
 
+	public static int suma(int tabla[]) {
+
+		int suma = 0;
+		
+		for (int numero : tabla) {
+			suma += numero;
+		}
+		
+		return suma;
+	}
 }
---
+++ ArraysFunciones/src/arraysfunciones/Ejercicio2.java
@@ -0,0 +1,16 @@
+package arraysfunciones;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		int t[] = {4, 2, 8, 6, 10};
+	}
+	
+	public static int maximo(int t[]) {
+	
+		
+	
+	}
+
+}
